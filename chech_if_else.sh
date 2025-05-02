3e2a673e2c5a51320284a4c97669ae149e480ab6
Ubuntu <ubuntu@ip-172-31-92-162.ec2.internal>
2025-05-02 13:13:02 +0000

    Add if-else script for Jehta example

--- chech_if_else.sh
+++ chech_if_else.sh
@@ -5,10 +5,16 @@ This is the if else statement
 disc
 
 read -p "Jehta ka pita kon hai: " papa
+read -p "jehta ka pyra apne pita ji ke liye %" pyaar
 
 if [[ $papa == "champak" ]];
 then	
 	echo "yeh hai jehta ka pita shree"
+elif [[ $pyaar -ge 90 ]];
+
+then
+	echo "Iske pita ji hi hai"
+
 else	
 	echo "Step Father"
 
